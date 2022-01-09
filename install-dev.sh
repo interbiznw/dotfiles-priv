309898b75fe66d5c296c31219366ebeb5640675f
J62 <j.owens@interbiznw.com>
2022-01-08 21:13:55 -0800

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -3,7 +3,7 @@
 sudo apt-get install -y \
       build-essential pkg-config libc6-dev m4 g++-multilib \
       autoconf libtool ncurses-dev unzip git python \
-      zlib1g-dev wget bsdmainutils automake docker.io rbenv golang-go
+      zlib1g-dev wget bsdmainutils automake docker.io rbenv golang-go docker-compose
       
 sudo usermod -aG docker $USER
 
